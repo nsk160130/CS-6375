f9ab15617c61ed477894a7acbe5028e1a243bb05
nsk160130 <niks.ce15@gmail.com>
2017-02-13 08:57:01 -0600

    Update Partition.py

--- Decision Tree/Partition.py
+++ Decision Tree/Partition.py
@@ -8,6 +8,7 @@ X3 = df["X3"]
 lst = [X1,X2,X3]
 Y = df["Class"]
 tree = []
+pDT = []
 level = 1
 global level2
 level2 = 0
@@ -26,7 +27,7 @@ def partition(level,target,lst,df,parent,value):
     Xp = [s for (p,s) in zip(X,Y)   if p == 1 ]
     Xn = [s for (p,s) in zip(X,Y)   if p == 0 ]
     
-    print "You should split on:",X.name
+    #print "You should split on:",X.name
     valuePa = "Parent Value:",value
     parentPa = "Parent is:",parent
    
@@ -40,16 +41,17 @@ def partition(level,target,lst,df,parent,value):
     else:
         XpN = 1
     for i in tree:
-        print i
+        #print i
+        pass
     tree.append([level,X.name,parentPa,valuePa,Xp,Xn,XpS,XpN])
     tada = (level,tree,X,Xp,"Split Needed on Left" if XpS else "Left Is Pure",Xn,"Split Needed on Right" if XpN else "Right Is Pure")
-    print "Split done:",(tada[4],tada[6])
+    #print "Split done:",(tada[4],tada[6])
     
     if tada[4] == 'Split Needed on Left':
         global level2
         level2 = tada[0] + 1
-        print "\n\n\n"
-        print "This is level",level+1
+        #print "\n\n\n"
+        #print "This is level",level+1
         df2 = df[tada[2]==1]
         Y2 = df2["Class"]
         X1 = df2["X1"]
@@ -63,8 +65,8 @@ def partition(level,target,lst,df,parent,value):
         
     if tada[6] == 'Split Needed on Right':
         level3 = level + 1
-        print "\n\n\n"
-        print "This is level",level+1
+        #print "\n\n\n"
+        #print "This is level",level+1
         df3 = df[tada[2]==0]
         Y3 = df3["Class"]
         X1 = df3["X1"]
@@ -76,3 +78,14 @@ def partition(level,target,lst,df,parent,value):
     else:
         pass
     return tree
+
+def printDT(tada = partition):
+    """
+    This function simply prints out the Tree.
+    """
+    for i in tada:
+        print "\n"
+        n = i[0] - 1
+        print n*'|',i[2][1],":",i[3][1],"Splited on:",i[1],"Splits: ","Left:Impure" if i[6] else "Left:Pure","and","Right:Impure" if i[7] else "Right:Pure","If",i[1]," = 0:","Next" if i[6] else i[4][0],"Else","If",i[1]," = 0:","Next" if i[7] else i[5][0]
+        pDT.append([n,i[2][1],":",i[3][1],"Splited on:",i[1],"Splits: ","Left:Impure" if i[6] else "Left:Pure","and","Right:Impure" if i[7] else "Right:Pure","If",i[1]," = 0:","Next" if i[6] else i[4][0],"Else","If",i[1]," = 0:","Next" if i[7] else i[5][0]])
+    return pDT
